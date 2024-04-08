65d51f921b8c134fdcb10903ee4926ae6cfe021a
Adrian Zavaleta <60676513+ZavaletaAdrian@users.noreply.github.com>
2024-04-08 17:39:48 -0600

    Added code for this

--- App.js
+++ App.js
@@ -6,17 +6,26 @@ export default function App() {
   useEffect(() => {
     Animated.timing(moonAnimation, {
       toValue: 1,
+      duration: 10000,
+      useNativeDriver: false,
     }).start();
   }, [moonAnimation]);
 
   const moonLeft = moonAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0%', '79.5%']
+    inputRange: [0,1],
+    outputRange: ['0%', '90%']
+  });
+
+  const moonColor = moonAnimation.interpolate({
+    inputRange: [0, 0.25, 0.5, 0.75, 1],
+    outputRange: ['yellow', 'orange', 'black', 'orange', 'yellow']
   });
 
   return (
     <View style={styles.container}>
       <Text>Eclipse 2024 🌒</Text>
+      <View style={styles.sun} /><View/>
+      <Animated.View style={[styles.moon, {left: moonLeft, backgroundColor: moonColor}]} />
     </View>
   );
 }
@@ -27,5 +36,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  moon: {
+    position: 'absolute',
+    bottom: '50%',
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    zIndex: 1,
+  },
+  sun: {
+    position: 'absolute',
+    bottom: '50%',
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: 'orange',
   },
 });
